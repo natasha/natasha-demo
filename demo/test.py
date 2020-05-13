6f826bac7bfbb3025cae936e1be0f7e3b46550d0
Kukushkin Alexander <alex@alexkuk.ru>
2020-05-13 15:02:45 +0300

    Test viz

--- demo/test.py
+++ demo/test.py
@@ -47,8 +47,42 @@ SPANS = [
      'type': 'PER'}
 ]
 
+MORPH = '''
+                 Так ADV|Degree=Pos
+            говорила VERB|Aspect=Imp|Gender=Fem|Mood=Ind|Number=Sing|Tense=Past|VerbForm=Fin|Voice=Act
+                   в ADP
+                июле NOUN|Animacy=Inan|Case=Loc|Gender=Masc|Number=Sing
+                1805 ADJ
+                года NOUN|Animacy=Inan|Case=Gen|Gender=Masc|Number=Sing
+           известная ADJ|Case=Nom|Degree=Pos|Gender=Fem|Number=Sing
+                Анна PROPN|Animacy=Anim|Case=Nom|Gender=Fem|Number=Sing
+            Павловна PROPN|Animacy=Anim|Case=Nom|Gender=Fem|Number=Sing
+               Шерер PROPN|Animacy=Anim|Case=Nom|Gender=Fem|Number=Sing
+                   , PUNCT
+            фрейлина NOUN|Animacy=Inan|Case=Nom|Gender=Masc|Number=Sing
+                   и CCONJ
+        приближенная NOUN|Animacy=Inan|Case=Dat|Gender=Neut|Number=Sing
+         императрицы NOUN|Animacy=Anim|Case=Gen|Gender=Fem|Number=Sing
+               Марии PROPN|Animacy=Anim|Case=Gen|Gender=Fem|Number=Sing
+          Феодоровны PROPN|Animacy=Anim|Case=Gen|Gender=Fem|Number=Sing
+                   , PUNCT
+            встречая VERB|Aspect=Imp|Tense=Pres|VerbForm=Conv|Voice=Act
+             важного ADJ|Animacy=Anim|Case=Acc|Degree=Pos|Gender=Masc|Number=Sing
+                   и CCONJ
+           чиновного ADJ|Case=Gen|Degree=Pos|Gender=Masc|Number=Sing
+               князя NOUN|Animacy=Anim|Case=Gen|Gender=Masc|Number=Sing
+             Василия PROPN|Animacy=Anim|Case=Gen|Gender=Masc|Number=Sing
+                   , PUNCT
+             первого ADJ|Case=Gen|Degree=Pos|Gender=Masc|Number=Sing
+         приехавшего VERB|Aspect=Perf|Case=Gen|Gender=Masc|Number=Sing|Tense=Past|VerbForm=Part|Voice=Act
+                  на ADP
+                  ее DET
+               вечер NOUN|Animacy=Inan|Case=Acc|Gender=Masc|Number=Sing
+                   . PUNCT
+'''[1:]
 
-async def test(client):
+
+async def test_spans(client):
     response = await client.post(
         '/api/doc/spans',
         data={'text': TEXT}
@@ -57,3 +91,14 @@ async def test(client):
     data = await response.json()
 
     assert data == SPANS
+
+
+async def test_viz(client):
+    response = await client.post(
+        '/api/doc/viz',
+        data={'text': TEXT}
+    )
+    assert response.status == 200
+    data = await response.json()
+
+    assert data['morph'] == MORPH
